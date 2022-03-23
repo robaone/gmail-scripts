ca671c476d5565a2a1ef8375fc4d26327e660202
Ansel Robateau <robaone@gmail.com>
2022-03-23 16:20:48 -0500

    Update RecoverInterestingMail.js

--- gapps/RecoverInterestingMail.js
+++ gapps/RecoverInterestingMail.js
@@ -1,10 +1,14 @@
 class RecoverInterestingMail {
-  constructor(properties){
-    this.BATCH_LIMIT = 100;
+  constructor(gmailApp, properties){
+    this.BATCH_LIMIT = properties?.limit ?? 100;
+    this.gmailApp = gmailApp;
   }
 
+  run() {
+
+  }
   mailSearch(search_string, properties) {
-    const threads = GmailApp.search(this.buildQuery(search_string,properties), 0, properties.limit ?? this.BATCH_LIMIT);
+    const threads = this.gmailApp.search(this.buildQuery(search_string,properties), 0, properties.limit ?? this.BATCH_LIMIT);
     return threads;
   }
   
@@ -16,14 +20,16 @@ class RecoverInterestingMail {
 
 class RecoverInterestingMailTest {
   constructor() {
-    
+    this.gmailApp = {};
   }
   before() {
-    this.service = new RecoverInterestingMail();
+    this.gmailApp = {};
+    this.service = new RecoverInterestingMail(this.gmailApp, { limit: 99 });
   }
 
   test_mailSearchWithoutLimit() {
     // GIVEN
+    this.gmailApp.search = (_) => new Array(99);
     const search_string = 'test';
     const properties = {
       after: Utilities.formatDate(new Date(), Session.getTimeZone(), "yyyy-MM-dd"),
@@ -36,10 +42,11 @@ class RecoverInterestingMailTest {
 
     // THEN
     console.log(result.length);
-    Assert(result.length).equals(100);
+    Assert(result.length).equals(99);
   }
   test_mailSearch() {
     // GIVEN
+    this.gmailApp.search = (_) => new Array(10);
     const search_string = 'test';
     const properties = {
       after: Utilities.formatDate(new Date(), Session.getTimeZone(), "yyyy-MM-dd"),
