f0bc50d37e2f6dde0faf72e7a4d22592094e290f
Ansel Robateau <robaone@gmail.com>
2022-03-23 16:03:06 -0500

    Update RecoverInterestingMail.js

--- gapps/RecoverInterestingMail.js
+++ gapps/RecoverInterestingMail.js
@@ -9,14 +9,42 @@ function buildQuery(search_string,properties) {
   return search_string;
 }
 
-function test_mailSearch() {
-  const search_string = 'test';
-  const properties = {
-    after: Utilities.formatDate(new Date(), Session.getTimeZone(), "yyyy-MM-dd"),
-    before: Utilities.formatDate(new Date(), Session.getTimeZone(), "yyyy-MM-dd"),
-    label: "unread",
-    limit: 10
-  };
-  const result = mailSearch(search_string,properties);
-  console.log(result.length);
+class RecoverInterestingMailTest {
+  test_mailSearchWithoutLimit() {
+    // GIVEN
+    const search_string = 'test';
+    const properties = {
+      after: Utilities.formatDate(new Date(), Session.getTimeZone(), "yyyy-MM-dd"),
+      before: Utilities.formatDate(new Date(), Session.getTimeZone(), "yyyy-MM-dd"),
+      label: "unread"
+    };
+
+    // WHEN
+    const result = mailSearch(search_string,properties);
+
+    // THEN
+    console.log(result.length);
+    Assert(result.length).equals(100);
+  }
+  test_mailSearch() {
+    // GIVEN
+    const search_string = 'test';
+    const properties = {
+      after: Utilities.formatDate(new Date(), Session.getTimeZone(), "yyyy-MM-dd"),
+      before: Utilities.formatDate(new Date(), Session.getTimeZone(), "yyyy-MM-dd"),
+      label: "unread",
+      limit: 10
+    };
+
+    // WHEN
+    const result = mailSearch(search_string,properties);
+
+    // THEN
+    console.log(result.length);
+    Assert(result.length).equals(10);
+  }
+}
+
+function test_recoverInterestingMail() {
+  new Test(new RecoverInterestingMailTest()).run();
 }
